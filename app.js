b854e83bd5e4ab024daa763c7e9a4239356ee01d
unknown <adikkumar11@gmail.com>
2020-02-01 22:28:08 +0530

    poorly_formatted_request

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const request = require("request");
-
 //var cooridinates;
-var placeName = "leh123";
-
-const url =
-  "https://api.darksky.net/forecast/6e9a671c783df23560afe2aef1c87f76/37.8267,-122.4233?units=si";
-
-//var url = "https://api.darksky.net/forecast/6e9a671c783df23560afe2aef1c87f76/";
+var placeName = "chennai";
+//const url =
+//"https://api.darksky.net/forecast/6e9a671c783df23560afe2aef1c87f76/37.8267,-122.4233?units=si";
+var url = "https://api.darksky.net/forecast/6e9a671c783df23560afe2aef1c87f76/";
 
 const place_search_url =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
   placeName +
   ".json?access_token=pk.eyJ1IjoiYWRpdHlha3VtYXItY29kZXMiLCJhIjoiY2s1emhqc3VuMnJ1NDNkbjVyejJrcDZvdSJ9.NVoqWRTtSMxOn3FZNbhAlw&limit=1";
+
+//******Geocoding request */
 
 request({ url: place_search_url, json: true }, (error, response) => {
   if (!!error) {
@@ -23,22 +22,20 @@ request({ url: place_search_url, json: true }, (error, response) => {
     console.log(data[1].toFixed(4) + "," + data[0].toFixed(4));
     var coordinates = data[1].toFixed(4) + "," + data[0].toFixed(4);
     console.log(coordinates);
-    //    url = url + coordinates + "?units=si";
+    url = url + coordinates + "?units=si";
   }
 });
 
-//console.log(coordinates);
+///***** Getting the weather */
 
 request({ url: url, json: true }, (error, response) => {
   if (error) {
     console.log("Tyler the error");
   } else if (response.body.error) {
-    console.log(url);
+    console.log(url); // when I copy this from the teminal and paste it on the browsser, it works perfectly
     console.log(response.body.error);
   } else {
     const data = response.body;
-    //console.log(data);
-
     console.log(
       response.body.daily.data[0].summary +
         " it is currently " +
